f716045915ac6a171f769838dcca535eb9c16f4e
xy8000 <lennartpezers@gmail.com>
2019-04-10 15:22:53 +0200

    Login und Registrieren mit DB anbindung

---
+++ html/registrationAndLogin/login.php
@@ -0,0 +1,31 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Login</h2>
+  </div>
+	 
+  <form method="post" action="login.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  		<label>Nutzername</label>
+  		<input type="text" name="username" >
+  	</div>
+  	<div class="input-group">
+  		<label>Passwort</label>
+  		<input type="password" name="password">
+  	</div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user">Anmelden</button>
+  	</div>
+  	<p>
+  		Noch nicht registriert? <a href="register.php">Sign up</a>
+  	</p>
+  </form>
+</body>
+</html>
