29c16e3d0dadb67d6cd7134bd1112509760b5909
xy8000 <lennartpezers@gmail.com>
2019-04-17 10:06:13 +0200

    Verbessern der Regestrier-Seite

--- html/registrationAndLogin/register.php
+++ html/registrationAndLogin/register.php
@@ -1,5 +1,138 @@
-<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
 
+<?php include_once('server.php') ?>
+<!-- Includes Bootstrap-->
+<link rel="stylesheet" href="/html/bootstrap/bootstrap.css">
+<script src="/html/bootstrap/bootstrap.bundle.js"></script>
+<script src="/html/bootstrap/bootstrap.bundle.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+<?php
+//Automatischer verweis auf die Homepage
+	if (nutzer_angemeldet()){
+		header('location: /html/homepage/index.php');
+	}
+?>
+
+<head>
+<style type="text/css">
+    body {
+		font-family: 'Varela Round', sans-serif;
+	}
+	.modal-login {
+		color: #636363;
+		width: 350px;
+	}
+	.modal-login .modal-content {
+		padding: 20px;
+		border-radius: 5px;
+		border: none;
+	}
+	.modal-login .modal-header {
+		border-bottom: none;
+		position: relative;
+		justify-content: center;
+	}
+	.modal-login h4 {
+		text-align: center;
+		font-size: 26px;
+	}
+	.modal-login  .form-group {
+		position: relative;
+	}
+	.modal-login i {
+		position: absolute;
+		left: 13px;
+		top: 11px;
+		font-size: 18px;
+	}
+	.modal-login .form-control {
+		padding-left: 40px;
+	}
+	.modal-login .form-control:focus {
+		border-color: #00ce81;
+	}
+	.modal-login .form-control, .modal-login .btn {
+		min-height: 40px;
+		border-radius: 3px; 
+	}
+	.modal-login .hint-text {
+		text-align: center;
+		padding-top: 10px;
+	}
+	.modal-login .close {
+        position: absolute;
+		top: -5px;
+		right: -5px;
+	}
+	.modal-login .btn {
+		background: #00ce81;
+		border: none;
+		line-height: normal;
+	}
+	.modal-login .btn:hover, .modal-login .btn:focus {
+		background: #00bf78;
+	}
+	.modal-login .modal-footer {
+		background: #ecf0f1;
+		border-color: #dee4e7;
+		text-align: center;
+		margin: 0 -20px -20px;
+		border-radius: 5px;
+		font-size: 13px;
+		justify-content: center;
+	}
+	.modal-login .modal-footer a {
+		color: #999;
+	}
+	.trigger-btn {
+		display: inline-block;
+		margin: 100px auto;
+	}
+</style>
+<body>
+    </form>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/html/homepage/header.php');?>
+	<div class="modal-dialog modal-login">
+		<div class="modal-content">
+			<div class="modal-header">				
+				<h4 class="modal-title">Registrieren</h4>
+			</div>
+			<div class="modal-body">
+				<form action="register.php" method="post">
+					<div class="form-group">
+						<i class="fa fa-user"></i>
+						<input type="text" name="username" class="form-control" placeholder="Username" required="required">
+					</div>
+                    <div class="form-group">
+						<i class="fa fa-user"></i>
+						<input type="email" name="email" class="form-control" placeholder="E-Mail" required="required">
+					</div>
+					<div class="form-group">
+						<i class="fa fa-lock"></i>
+						<input type="password" name="password_1" class="form-control" placeholder="Password" required="required">					
+					</div>
+                    <div class="form-group">
+						<i class="fa fa-lock"></i>
+						<input type="password" name="password_2" class="form-control" placeholder="Password" required="required">					
+					</div>
+					<div class="form-group">
+						<input type="submit" name="reg_user" class="btn btn-primary btn-block btn-lg" value="Register">
+					</div>
+				</form>					
+			</div>
+			<div class="modal-footer">
+				<a href="login.php">Schon Mitglied? Anmelden</a>
+			</div>
+		</div>
+    </div>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/html/homepage/footer.php');?>   
+</body>
+<!-- Alte form
 <form action="register.php" method="post">
 	<?php include('errors.php'); ?>
   	<div class="input-group">
@@ -24,4 +157,5 @@
   	<p>
   		Already a member? <a href="login.php">Sign in</a>
   	</p>
-</form>
+</form>-->
+</html>
