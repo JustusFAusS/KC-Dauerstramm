235df24161e77f15215ffdc04164a11bbfa463f4
xy8000 <lennartpezers@gmail.com>
2019-04-17 11:17:18 +0200

    Überschriften farblich angepasst

--- html/homepage/footer.php
+++ html/homepage/footer.php
@@ -12,14 +12,14 @@
                         <small class="d-block mb-3 text-muted">© 2018-2019</small>
                     </div>
                     <div class="col-6 col-md">
-                        <h5>Links</h5>
+                        <h5 style="color:white">Links</h5>
                         <ul class="list-unstyled text-small">
                         <li><a class="text-muted" href="/html/homepage/index.php">Homepage</a></li>
                         <li><a class="text-muted" href="#">Regeln</a></li>
                         </ul>
                     </div>
                     <div class="col-6 col-md">
-                        <h5>About</h5>
+                        <h5 style="color:white">About</h5>
                         <ul class="list-unstyled text-small">
                         <li><a class="text-muted" href="#">Team</a></li>
                         <li><a class="text-muted" href="#">Locations</a></li>
