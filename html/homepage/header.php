66769272c798a88645cded91186e16c91fd653ef
7984 <48592359+KP7984@users.noreply.github.com>
2019-04-18 12:20:57 +0200

    ordnerstruktur

--- html/homepage/header.php
+++ html/homepage/header.php
@@ -14,7 +14,7 @@
         <!-- Links -->
         <ul class="navbar-nav mr-auto">
             <li class="nav-item">
-                <a class="nav-link" href="/html/homepage/images.php">Bilder</a>
+                <a class="nav-link" href="/KCD/html/homepage/images.php">Bilder</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Link 3</a>
