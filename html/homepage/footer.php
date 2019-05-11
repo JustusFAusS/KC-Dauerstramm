1b533de0b8c70eba37f70bf1543aa8e9bdae824a
xy8000 <lennartpezers@gmail.com>
2019-05-11 14:12:38 +0200

    Anpassungen im Footer und Header

--- html/homepage/footer.php
+++ html/homepage/footer.php
@@ -9,22 +9,22 @@
                     <div class="col-12 col-md">
                         <img class="mb-2" src="/KCD/resources/images/homepage/HomepageIcon.png" alt="" width="40" height="40">
                         <small class="d-block mb-3 text-muted">Justus Falke, Marius Hilt, Lennart Peters</small>
-                        <small class="d-block mb-3 text-muted">© 2018-2019</small>
+                        <small class="d-block mb-3 text-muted">© 2019-2020</small>
                     </div>
                     <div class="col-6 col-md">
                         <h5 style="color:white">Links</h5>
                         <ul class="list-unstyled text-small">
                         <li><a class="text-muted" href="/KCD/html/homepage/index.php">Homepage</a></li>
-                        <li><a class="text-muted" href="#">Regeln</a></li>
+                        <li><a class="text-muted" href="/KCD/html/homepage/images.php">Bilder</a></li>
                         </ul>
                     </div>
                     <div class="col-6 col-md">
                         <h5 style="color:white">About</h5>
                         <ul class="list-unstyled text-small">
-                        <li><a class="text-muted" href="#">Team</a></li>
-                        <li><a class="text-muted" href="#">Locations</a></li>
-                        <li><a class="text-muted" href="#">Privacy</a></li>
-                        <li><a class="text-muted" href="#">Terms</a></li>
+                        <li><a class="text-muted" >Team</a></li>
+                        <li><a class="text-muted" >Locations</a></li>
+                        <li><a class="text-muted" >Privacy</a></li>
+                        <li><a class="text-muted" >Terms</a></li>
                         </ul>
                     </div>
                 </div>
