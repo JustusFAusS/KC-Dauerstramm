80929217c3b0bcd2f0ce1825d67a562ada071a9d
xy8000 <lennartpezers@gmail.com>
2019-05-17 22:06:57 +0200

    Strafe erintragen zu Strafe erfassen umbenannt

--- html/homepage/header.php
+++ html/homepage/header.php
@@ -61,7 +61,7 @@
                     if (checkKassenwartPermissions($userID,$db))
                     {
                         echo '<a class="dropdown-item" href="/KCD/html/penalties/edit_penalties.php">Strafen verwalten</a>';
-                        echo '<a class="dropdown-item" href="/KCD/html/penalties/add_penalty.php">Strafen einreichen</a>';
+                        echo '<a class="dropdown-item" href="/KCD/html/penalties/add_penalty.php">Strafen erfassen</a>';
                         echo '<a class="dropdown-item" href="/KCD/html/penalties/create_penalty.php">Strafe erstellen</a>';
                         echo '<a class="dropdown-item" href="/KCD/html/penalties/delete_penalty.php">Strafe löschen</a>';
                     }
