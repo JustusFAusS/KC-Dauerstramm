71688a527a46f971453757c22adfe45da0b3f00a
JustusFAusS <49314103+JustusFAusS@users.noreply.github.com>
2019-06-01 15:15:23 +0200

    Vermeidung von XAMPP Fehlermeldungen bei UploadImage

--- html/uploadImage/upload_image.php
+++ html/uploadImage/upload_image.php
@@ -7,9 +7,6 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <html>
-<head>
-    <title>Bild hochladen</title>
-</head>
 <body>
     <?php include_once('service_upload_image.php') ?>
     <?php include($_SERVER['DOCUMENT_ROOT'] . '/KCD/html/global/header.php');?>
@@ -23,11 +20,11 @@
 				<form action="upload_image.php" method="post" enctype="multipart/form-data">
 					<div class="form-group">
 						<i class="fa fa-user"></i>
-						<input type="text" name="title" class="form-control" placeholder="Titel" required="required" value="<?php echo $target_title; ?>">
+						<input type="text" name="title" class="form-control" placeholder="Titel" required="required" >
 					</div>
                     <div class="form-group">
 						<i class="fa fa-envelope"></i>
-						<input type="text" name="comment" class="form-control" placeholder="Kommentar" required="required" value="<?php echo $target_comment; ?>">
+						<input type="text" name="comment" class="form-control" placeholder="Kommentar" required="required" >
 					</div>
 					<div class="form-group">
 						<input type="file" name="fileToUpload" id="fileToUpload">
